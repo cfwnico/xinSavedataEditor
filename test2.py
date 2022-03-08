86dde52944bd2b333e6873dab3cdcfcafa36dddc
cfwy7000 <caofan2017@gmail.com>
2022-03-08 15:11:53 +0800

    检查变量

--- test2.py
+++ test2.py
@@ -2,14 +2,29 @@ from glob import glob
 from pyamf import sol
 import os
 
-path = r"~\AppData\Roaming\Macromedia\Flash Player\#SharedObjects\**\*.sol"
-path = os.path.expanduser(path)
-sol_file_list = glob(path, recursive=True)
-file_name = sol_file_list[0]
-f = sol.load(file_name)
 
-a = f["save1tonameofstage"]
+def find_last_edit_sol():
+    path = r"~\AppData\Roaming\Macromedia\Flash Player\#SharedObjects\**\*.sol"
+    path = os.path.expanduser(path)
+    sol_file_list = glob(path, recursive=True)
+    if len(sol_file_list) == 0:
+        return
+    edit_times = []
+    for file_name in sol_file_list:
+        mtime = os.path.getmtime(file_name)
+        edit_times.append(mtime)
+    max_edit_time = max(edit_times)
+    file_name = edit_times.index(max_edit_time)
+    last_edit_file = sol_file_list[file_name]
+    last_edit_file = os.path.abspath(last_edit_file)
+    return last_edit_file
+
+
+c = find_last_edit_sol()
+f = sol.load(c)
+
+a = f["save1heavyda"]
 print(a)
 
-# f["save1control"] = 2
-# f.save(file_name)
+f["save1heavyda"] = 100
+f.save(c)
